ff5d4f1761cbc00aec2d3a3fe67e0d71dd4b2ea1
mikus-loli <18726963417@qq.com>
2026-04-30 01:12:02 +0800

    refactor: 移除未使用的express.urlencoded中间件

--- src/index.js
+++ src/index.js
@@ -30,7 +30,6 @@ async function start() {
   app.use(helmet({ contentSecurityPolicy: false }));
   app.use(cors());
   app.use(express.json({ limit: '1mb' }));
-  app.use(express.urlencoded({ extended: true, limit: '1mb' }));
 
   const limiter = rateLimit({
     windowMs: config.rateLimitWindowMs,
